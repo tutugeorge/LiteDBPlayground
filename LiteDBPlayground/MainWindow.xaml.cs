43f0ef9e2a0c621d44b8f3885676a680cd3a311f
Tutu George <tutugeorge@outlook.com>
2017-08-16 20:30:31 +0530

    Added DB

--- LiteDBPlayground/MainWindow.xaml.cs
+++ LiteDBPlayground/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,20 @@ namespace LiteDBPlayground
         public MainWindow()
         {
             InitializeComponent();
+            InitDB();
+        }
+
+        void InitDB()
+        {
+            SQLiteConnection con = new SQLiteConnection("Data Source=main.db");
+
+            con.Open();
+            var cmd = con.CreateCommand();
+
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS tab1(id int, name varchar(30), age int)";
+            cmd.ExecuteNonQuery();
+
+            con.Close();
         }
     }
 }
